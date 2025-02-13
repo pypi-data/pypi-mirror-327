7ffcec565a747b2cad4d2d37bd96552e856f6ebc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 04:03:58 +0000

    Add package motile-tracker/3.1.0.dev0/motile_tracker-3.1.0.dev0.tar.gz

---
+++ packages/motile-tracker/motile_tracker-3.1.0.dev0.tar.gz/motile_tracker-3.1.0.dev0/scripts/view_external_tracks.py
@@ -0,0 +1,26 @@
+import napari
+from motile_tracker.application_menus import MainApp
+from motile_tracker.data_views.views_coordinator.tracks_viewer import TracksViewer
+from motile_tracker.example_data import Fluo_N2DL_HeLa
+from motile_tracker.utils.load_tracks import tracks_from_csv
+
+if __name__ == "__main__":
+    # load the example data
+    raw_layer_info, labels_layer_info, points_layer_info = Fluo_N2DL_HeLa()
+    segmentation_arr = labels_layer_info[0]
+    # the segmentation ids in this file correspond to the segmentation ids in the
+    # example segmentation data, loaded above
+    csvfile = "hela_example_tracks.csv"
+    tracks = tracks_from_csv(csvfile, segmentation_arr)
+
+    viewer = napari.Viewer()
+    raw_data, raw_kwargs, _ = raw_layer_info
+    viewer.add_image(raw_data, **raw_kwargs)
+    labels_data, labels_kwargs, _ = labels_layer_info
+    viewer.add_labels(labels_data, **labels_kwargs)
+    widget = MainApp(viewer)
+    viewer.window.add_dock_widget(widget)
+    TracksViewer.get_instance(viewer).tracks_list.add_tracks(tracks, "example")
+
+    # Start the Napari GUI event loop
+    napari.run()
