683769e1a7a5d39bd140e20f625f866bb8f93468
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 04:15:47 +0000

    Add package flowpulse/1.1.3/flowpulse-1.1.3-py3-none-any.whl

---
+++ packages/flowpulse/flowpulse-1.1.3-py3-none-any.whl/flowpulse/WorkItem.py
@@ -0,0 +1,32 @@
+from datetime import datetime
+
+class WorkItem:
+    def __init__(self, id, title, started_date, closed_date, estimation):
+        self.id = id    
+        self.started_date = None
+        self.closed_date = None
+        self.title = title
+        self.estimation = estimation
+        
+        self.item_title = id
+        
+        self.work_item_age = None
+        self.cycle_time = None
+        self.closed_date = closed_date
+        self.started_date = started_date
+        
+        if self.started_date and self.closed_date:
+            self.cycle_time = (self.closed_date - self.started_date).days + 1
+        elif started_date and not self.closed_date:
+            self.work_item_age = (datetime.today() - self.started_date).days + 1
+
+    def to_dict(self):
+        result = {
+            'work_item_age': self.work_item_age,
+            'cycle_time': self.cycle_time,
+        }
+        if self.started_date:
+            result['started_date'] = self.started_date.date()
+        if self.closed_date:
+            result['closed_date'] = self.closed_date.date()
+        return result
