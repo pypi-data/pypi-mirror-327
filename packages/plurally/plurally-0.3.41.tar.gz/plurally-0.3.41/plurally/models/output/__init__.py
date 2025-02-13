8d7d045f1001e9a6d55c493d22ef8de8851e1a47
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 06:05:03 +0000

    Add package plurally/0.3.41/plurally-0.3.41.tar.gz

---
+++ packages/plurally/plurally-0.3.41.tar.gz/plurally-0.3.41/plurally/models/output/__init__.py
@@ -0,0 +1,6 @@
+
+from .text import TextOutput, HtmlOutput
+from .crm_actions import CrmActionsOutput
+from .table import TableOutput
+
+__all__ = ["TextOutput", "CrmActionsOutput", "HtmlOutput", "TableOutput"]
