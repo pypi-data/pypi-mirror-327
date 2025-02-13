cffa12cbead1a031779319914b4aab940e74b30a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:04:37 +0000

    Add package hockey-blast-common-lib/0.1.38/hockey_blast_common_lib-0.1.38-py3-none-any.whl

---
+++ packages/hockey-blast-common-lib/hockey_blast_common_lib-0.1.38-py3-none-any.whl/hockey_blast_common_lib/options.py
@@ -0,0 +1,23 @@
+import argparse
+
+MAX_HUMAN_SEARCH_RESULTS = 25
+MAX_TEAM_SEARCH_RESULTS = 25
+MIN_GAMES_FOR_ORG_STATS = 1
+MIN_GAMES_FOR_DIVISION_STATS = 1
+MIN_GAMES_FOR_LEVEL_STATS = 20
+
+orgs = {'caha', 'sharksice', 'tvice'}
+
+not_human_names = [
+    ("Away", None, None),
+    (None, "Unknown", None),
+    ("Not", None , None),
+    (None , None, "Goalie")
+]
+
+def parse_args():
+    parser = argparse.ArgumentParser(description="Process data for a specific organization.")
+    parser.add_argument("org", choices=orgs, help="The organization to process (e.g., 'caha', 'sharksice', 'tvice').")
+    parser.add_argument("--reprocess", action="store_true", help="Reprocess existing data.")
+    parser.add_argument("--pre_process", action="store_true", help="Pre-Process existing data.")
+    return parser.parse_args()
