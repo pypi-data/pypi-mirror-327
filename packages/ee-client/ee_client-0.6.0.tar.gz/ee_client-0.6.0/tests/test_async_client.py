726c9e473f6565d6ff625b393015c2c97b7ce670
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 06:05:16 +0000

    Add package ee-client/0.6.0/ee_client-0.6.0.tar.gz

---
+++ packages/ee-client/ee_client-0.6.0.tar.gz/ee_client-0.6.0/tests/test_async_client.py
@@ -0,0 +1,23 @@
+import time
+from eeclient.async_client import AsyncEESession
+from eeclient.data import get_assets_async
+import asyncio
+
+# get sepal_headers from sepal
+async_session = AsyncEESession(sepal_headers)
+
+
+async def test_get_assets_async():
+    return await get_assets_async(
+        async_client=async_session,
+        folder=f"projects/{async_session.project_id}/assets/",
+    )
+
+
+if __name__ == "__main__":
+    start_time = time.time()
+    results = asyncio.run(test_get_assets_async())
+    end_time = time.time()
+    duration = end_time - start_time
+    print(results)
+    print(f"Time taken: {duration} seconds")
