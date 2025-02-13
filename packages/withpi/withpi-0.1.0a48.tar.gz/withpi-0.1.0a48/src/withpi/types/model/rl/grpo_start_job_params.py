ed13f16159cd851150d48369313bc3898fc6ad20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:04:50 +0000

    Add package withpi/0.1.0a48/withpi-0.1.0a48.tar.gz

---
+++ packages/withpi/withpi-0.1.0a48.tar.gz/withpi-0.1.0a48/src/withpi/types/model/rl/grpo_start_job_params.py
@@ -0,0 +1,32 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from __future__ import annotations
+
+from typing import Iterable
+from typing_extensions import Literal, Required, TypedDict
+
+from ...shared_params.contract import Contract
+
+__all__ = ["GrpoStartJobParams", "Example"]
+
+
+class GrpoStartJobParams(TypedDict, total=False):
+    contract: Required[Contract]
+    """The contract to use in the GRPO tuning process"""
+
+    examples: Required[Iterable[Example]]
+    """Examples to use in the RL tuning process"""
+
+    model: Required[Literal["LLAMA_3.2_1B"]]
+    """The model to start the RL process"""
+
+    learning_rate: float
+    """SFT learning rate"""
+
+    num_train_epochs: int
+    """SFT number of train epochs"""
+
+
+class Example(TypedDict, total=False):
+    llm_input: Required[str]
+    """The input to LLM"""
