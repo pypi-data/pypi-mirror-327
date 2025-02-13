7bb7f10325309235c10aa769ff234f9078e9a5b5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:04:41 +0000

    Add package multi-crypto-exchanges-api/1.1.3/multi_crypto_exchanges_api-1.1.3.tar.gz

---
+++ packages/multi-crypto-exchanges-api/multi_crypto_exchanges_api-1.1.3.tar.gz/multi_crypto_exchanges_api-1.1.3/cli.py
@@ -0,0 +1,28 @@
+import uvicorn
+import typer
+import subprocess
+import os, sys
+
+app = typer.Typer()
+
+@app.command()
+def run(command: str):
+    """
+    Lancer une commande personnalisée.
+    
+    Utilisez 'server' pour lancer le serveur via uvicorn,
+    ou 'streamlit' pour lancer l'application Streamlit.
+    """
+    if command == "server":
+        typer.echo("Lancement du serveur...")
+        sys.path.insert(0, os.path.join(os.path.dirname(__file__), "Server"))
+        uvicorn.run("Server:app", host="0.0.0.0", port=8000)
+    elif command == "streamlit":
+        typer.echo("Lancement de l'application Streamlit...")
+        # Lance le script Streamlit, ici situé dans le dossier Client
+        subprocess.run(["streamlit", "run", "Client/StreamlitClient.py"])
+    else:
+        typer.echo(f"Commande inconnue : {command}")
+
+if __name__ == "__main__":
+    app()
