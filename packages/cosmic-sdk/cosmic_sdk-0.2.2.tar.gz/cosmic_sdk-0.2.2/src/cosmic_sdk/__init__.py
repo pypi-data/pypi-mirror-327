c9b32c3db23ee50652bd7e93408c941365ec2f51
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:04:49 +0000

    Add package cosmic-sdk/0.2.2/cosmic_sdk-0.2.2.tar.gz

---
+++ packages/cosmic-sdk/cosmic_sdk-0.2.2.tar.gz/cosmic_sdk-0.2.2/src/cosmic_sdk/__init__.py
@@ -0,0 +1,9 @@
+from cosmic_sdk.connectors import PostHogService
+from cosmic_sdk.secrets import SecretsManager
+
+__version__ = "0.0.1"
+
+__all__ = [
+    "PostHogService",
+    "SecretsManager"
+]
