6d5653fa6c6fb990f6432d20a3f5ff5301b2f80b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 04:03:56 +0000

    Add package ICEBEAR/0.1.0/icebear-0.1.0.tar.gz

---
+++ packages/ICEBEAR/icebear-0.1.0.tar.gz/icebear-0.1.0/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ICEBEAR",
+    version="0.1.0",
+    packages=find_packages(),
+    description="ENOTION",
+    author="Chione Raphael",
+    license="MIT",
+)
