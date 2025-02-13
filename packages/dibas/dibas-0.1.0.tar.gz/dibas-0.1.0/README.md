00e521d57e815766470684f6f76a927aa7618f76
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:04:29 +0000

    Add package dibas/0.1.0/dibas-0.1.0.tar.gz

---
+++ packages/dibas/dibas-0.1.0.tar.gz/dibas-0.1.0/README.md
@@ -0,0 +1,21 @@
+# dibas-pip
+
+A simple Python package that displays "dibas" when the showName method is called.
+
+## Installation
+
+```bash
+pip install dibas-pip
+```
+
+## Usage
+
+```python
+from dibas_pip import showName
+
+showName()  # Outputs: dibas
+```
+
+## License
+
+This project is licensed under the MIT License.
---
+++ packages/dibas/dibas-0.1.0.tar.gz/dibas-0.1.0/setup.py
@@ -0,0 +1,17 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="dibas",
+    version="0.1.0",
+    packages=find_packages(),
+    description="A simple package that displays Dibas",
+    author="Dibas",
+    author_email="dibasdauliya@gmail.com",
+    url="https://github.com/dibasdauliya/dibas-pip",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6"
+)
