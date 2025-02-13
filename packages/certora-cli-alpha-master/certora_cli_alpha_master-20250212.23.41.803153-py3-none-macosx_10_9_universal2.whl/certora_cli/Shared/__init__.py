44911da177f9026d5598086daf173315c00d2ddc
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:04:32 +0000

    Add package certora-cli-alpha-master/20250212.23.41.803153/certora_cli_alpha_master-20250212.23.41.803153-py3-none-macosx_10_9_universal2.whl

---
+++ packages/certora-cli-alpha-master/certora_cli_alpha_master-20250212.23.41.803153-py3-none-macosx_10_9_universal2.whl/certora_cli/Shared/__init__.py
@@ -0,0 +1,7 @@
+"""
+This package has files that contain utility functions which are used by all the other packages.
+Specifically, if you have a functionality used by both CI_tests and CertoraProver, it should probably belong to one of the
+files under the current package.
+
+This package is a part of the certora_cli customer-facing Python package.
+"""
