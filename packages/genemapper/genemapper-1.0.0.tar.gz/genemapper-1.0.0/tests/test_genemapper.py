787fb6f018c251d682c1117e157c573b99dddd4f
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 03:26:54 +0000

    Add package genemapper/1.0.0/genemapper-1.0.0.tar.gz

---
+++ packages/genemapper/genemapper-1.0.0.tar.gz/genemapper-1.0.0/tests/test_genemapper.py
@@ -0,0 +1,21 @@
+#!/usr/bin/env python
+
+"""Tests for `genemapper` package."""
+
+
+import unittest
+
+from genemapper import genemapper
+
+
+class TestGenemapper(unittest.TestCase):
+    """Tests for `genemapper` package."""
+
+    def setUp(self):
+        """Set up test fixtures, if any."""
+
+    def tearDown(self):
+        """Tear down test fixtures, if any."""
+
+    def test_000_something(self):
+        """Test something."""
