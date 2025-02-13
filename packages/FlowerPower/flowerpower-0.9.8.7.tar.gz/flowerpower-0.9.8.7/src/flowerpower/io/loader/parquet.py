ee7b071976d439fe8a1866ab62c874f2bded87a2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 04:38:12 +0000

    Add package FlowerPower/0.9.8.7/flowerpower-0.9.8.7.tar.gz

---
+++ packages/FlowerPower/flowerpower-0.9.8.7.tar.gz/flowerpower-0.9.8.7/src/flowerpower/io/loader/parquet.py
@@ -0,0 +1,19 @@
+from ..base import BaseDatasetLoader, BaseFileLoader
+
+
+class ParquetFileLoader(BaseFileLoader):
+    def model_post_init(self, __context):
+        super().model_post_init(__context)
+        self.format = "parquet"
+
+    def __call__(self, **kwargs):
+        return self.to_pyarrow_table(**kwargs)
+
+
+class ParquetDatasetLoader(BaseDatasetLoader):
+    def model_post_init(self, __context):
+        super().model_post_init(__context)
+        self.format = "parquet"
+
+    def __call__(self, **kwargs):
+        return self.to_pyarrow_dataset(**kwargs)
