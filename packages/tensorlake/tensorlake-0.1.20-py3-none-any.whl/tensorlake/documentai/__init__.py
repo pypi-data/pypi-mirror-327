73892ff1dc14c45e844649a3b25a65c13681486d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 04:26:52 +0000

    Add package tensorlake/0.1.20/tensorlake-0.1.20-py3-none-any.whl

---
+++ packages/tensorlake/tensorlake-0.1.20-py3-none-any.whl/tensorlake/documentai/__init__.py
@@ -0,0 +1,10 @@
+from tensorlake.documentai.client import DocumentAI, ExtractionOptions, ParsingOptions
+from tensorlake.documentai.common import Document, JobResult
+
+__all__ = [
+    "DocumentAI",
+    "ParsingOptions",
+    "ExtractionOptions",
+    "Document",
+    "JobResult",
+]
