d0129c6157ba2294f49af0301908928f8b1fedf5
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 05:04:49 +0000

    Add package batch-id/0.1/batch_id-0.1.tar.gz

---
+++ packages/batch-id/batch_id-0.1.tar.gz/batch_id-0.1/batch_id/__init__.py
@@ -0,0 +1,1 @@
+from.main import get_Day,get_Day_Cal
---
+++ packages/batch-id/batch_id-0.1.tar.gz/batch_id-0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='batch_id',
+    version='0.1',
+    packages=find_packages(),
+    install_requires=[
+
+    ],
+)
