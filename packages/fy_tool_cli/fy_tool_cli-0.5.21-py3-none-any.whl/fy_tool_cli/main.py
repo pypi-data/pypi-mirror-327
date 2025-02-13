aa8da49b5281991f21732150d9acc340c99d3bf9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:04:29 +0000

    Add package fy_tool_cli/0.5.21/fy_tool_cli-0.5.21-py3-none-any.whl

---
+++ packages/fy_tool_cli/fy_tool_cli-0.5.21-py3-none-any.whl/fy_tool_cli/main.py
@@ -0,0 +1,48 @@
+# This Source Code Form is subject to the terms of the Mozilla Public
+# License, v. 2.0. If a copy of the MPL was not distributed with this
+#  file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
+import sys
+from pathlib import Path
+
+from fy_library import fy, fc
+
+
+def fy_cli() -> int:
+    folder_to_parse = "."
+    project_root_folder: str | None = None
+    if len(sys.argv) > 1:
+        folder_to_parse_index = 1
+        if sys.argv[1] == "--root":
+            folder_to_parse_index = 3
+            project_root_folder = sys.argv[2]
+        folder_to_parse = sys.argv[folder_to_parse_index]
+
+    try:
+        fc_index = sys.argv.index("--fc")
+    except ValueError:
+        fc_index = -1
+    if fc_index != -1:
+        fc(
+            folder_to_generate=Path(sys.argv[fc_index + 1]),
+            folder_to_parse=Path(folder_to_parse),
+            project_root_folder=(
+                Path(project_root_folder)
+                if project_root_folder is not None
+                else Path.cwd()
+            ),
+        )
+    else:
+        fy(
+            folder_to_parse=Path(folder_to_parse),
+            project_root_folder=(
+                Path(project_root_folder)
+                if project_root_folder is not None
+                else Path.cwd()
+            ),
+        )
+    return 0
+
+
+if __name__ == "__main__":
+    sys.exit(fy_cli())
