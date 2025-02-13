b68931d529bbad98ed29c0361ae1b04b5d8f3d31
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:04:26 +0000

    Add package opteryx/0.19.2b1092/opteryx-0.19.2b1092-cp310-cp310-win_amd64.whl

---
+++ packages/opteryx/opteryx-0.19.2b1092-cp310-cp310-win_amd64.whl/opteryx/virtual_datasets/no_table_data.py
@@ -0,0 +1,39 @@
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# See the License at http://www.apache.org/licenses/LICENSE-2.0
+# Distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND.
+
+"""
+no table
+---------
+
+This is used to prepresent no table.
+
+It actually is a table, with one row and one column.
+"""
+
+from orso.schema import FlatColumn
+from orso.schema import RelationSchema
+from orso.types import OrsoTypes
+
+from opteryx.models import RelationStatistics
+
+__all__ = ("read", "schema")
+
+
+def read(*args):
+    import pyarrow
+
+    # Create a PyArrow table with one column and one row
+
+    return pyarrow.Table.from_arrays([[0]], ["$column"])  # schema=_schema)
+
+
+def schema():
+    # fmt:off
+    return RelationSchema(name="$no_table", columns=[FlatColumn(name="$column", type=OrsoTypes.INTEGER)])
+    # fmt:on
+
+
+def statistics() -> RelationStatistics:
+    return RelationStatistics()
