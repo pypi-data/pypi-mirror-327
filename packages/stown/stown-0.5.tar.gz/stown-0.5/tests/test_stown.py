9e19e8f390ec38433890f2edaf189cc50d333686
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:04:25 +0000

    Add package stown/0.5/stown-0.5.tar.gz

---
+++ packages/stown/stown-0.5.tar.gz/stown-0.5/tests/test_stown.py
@@ -0,0 +1,113 @@
+"""
+Copyright © 2025 Ralph Seichter
+
+This file is part of "stown".
+
+stown is free software: you can redistribute it and/or modify it under the
+terms of the GNU General Public License as published by the Free Software
+Foundation, either version 3 of the License, or (at your option) any later
+version.
+
+stown is distributed in the hope that it will be useful, but WITHOUT ANY
+WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License along with
+stown. If not, see <https://www.gnu.org/licenses/>.
+"""
+
+from stown import __main__ as stown
+from typing import List
+import argparse
+import json
+import os
+import shutil
+import subprocess
+import unittest
+import uuid
+
+TMPDIR = "tmp"
+
+
+def getenv(key, default=None):
+    if key in os.environ:
+        return os.environ[key]
+    return default
+
+
+def truthy(x) -> bool:
+    return x or x == 1 or x == "true" or x == "yes"
+
+
+def load_json(path):
+    with open(path, "rt") as f:
+        return json.load(f)
+
+
+def random_tmp() -> str:
+    return os.path.join(TMPDIR, str(uuid.uuid4()))
+
+
+class TestStown(unittest.TestCase):
+    def setUp(self):
+        os.chdir(os.path.dirname(__file__))
+        self.datadir = "data"
+        self.args = self.parse_args()
+        if os.path.exists(TMPDIR):
+            shutil.rmtree(TMPDIR)
+        os.mkdir(TMPDIR)
+        os.mkdir(os.path.join(TMPDIR, "healthy"))
+
+    def parse_args(self, flags: List[str] = ["--verbose"]) -> argparse.Namespace:
+        return stown.arg_parser().parse_args(flags + [TMPDIR, self.datadir])
+
+    def tearDown(self):
+        pass
+
+    def assert_json_equal(self, checkme: str, expected: str):
+        c = load_json(checkme)
+        x = load_json(expected)
+        self.assertEqual(c, x)
+
+    def test_fail(self):
+        self.assertEqual(stown.fail("dummy", -42), -42)
+
+    def test_linkto_existing(self):
+        self.assertEqual(stown.linkto(self.args, ".", self.datadir), 2)
+
+    def test_linkto_existing_force(self):
+        rnd = random_tmp()
+        with open(rnd, "wt") as f:
+            print(rnd, file=f)
+        a = self.parse_args(["--force"])
+        self.assertEqual(stown.linkto(a, rnd, "expected.json"), 0)
+
+    def test_maxdepth(self):
+        a = self.parse_args(["--depth", "0"])
+        self.assertEqual(stown.stown(a, "x", "y"), 3)
+
+    def test_linkto_new(self):
+        self.assertEqual(stown.linkto(self.args, random_tmp(), "expected.json"), 0)
+
+    def test_parsed_fn1(self):
+        self.assertEqual(stown.parsed_filename("dot-foo"), ".foo")
+
+    def test_parsed_fn2(self):
+        self.assertEqual(stown.parsed_filename("bar"), "bar")
+
+    def test_same_file(self):
+        self.assertEqual(stown.stown(self.args, ".", "."), 4)
+
+    def test_stown(self):
+        self.assertEqual(stown.stown(self.args, self.args.target, self.args.source), 0)
+        if not truthy(getenv("DISABLE_TREE")):
+            subprocess.run(["tree", "-aJ", "-o", "tmp.json", self.args.target])
+            self.assert_json_equal("tmp.json", "expected.json")
+
+    def test_unstow(self):
+        a = self.parse_args(["--action", "unstow"])
+        self.assertEqual(stown.stown(a, "x", "y"), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
