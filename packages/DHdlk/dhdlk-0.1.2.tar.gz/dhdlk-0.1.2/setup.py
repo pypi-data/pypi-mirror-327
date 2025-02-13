8b45c632ff353732f284cf6cf68f62950f856139
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 05:04:34 +0000

    Add package DHdlk/0.1.2/dhdlk-0.1.2.tar.gz

---
+++ packages/DHdlk/dhdlk-0.1.2.tar.gz/dhdlk-0.1.2/setup.py
@@ -0,0 +1,21 @@
+from setuptools import setup, find_packages
+
+# README.md içeriğini güvenli bir şekilde oku
+with open("README.md", encoding="utf-8") as f:
+    long_description = f.read()
+
+setup(
+    name="DHdlk",
+    version="0.1.2",
+    packages=find_packages(),  # Paketleri otomatik bul
+    author="sahin",
+    description="Algebra library",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
