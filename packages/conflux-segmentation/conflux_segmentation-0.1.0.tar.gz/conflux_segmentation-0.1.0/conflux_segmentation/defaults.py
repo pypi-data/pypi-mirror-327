601fc0e64eaeb2ae480df74a2d23bb4cb74577f7
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 06:05:18 +0000

    Add package conflux-segmentation/0.1.0/conflux_segmentation-0.1.0.tar.gz

---
+++ packages/conflux-segmentation/conflux_segmentation-0.1.0.tar.gz/conflux_segmentation-0.1.0/conflux_segmentation/defaults.py
@@ -0,0 +1,10 @@
+from .types import ActivationType, BlendModeType
+
+DEFAULT_NUM_CLASSES: int = 1
+DEFAULT_TILE_SIZE: int = 512
+DEFAULT_OVERLAP: float = 0.125
+DEFAULT_BLEND_MODE: BlendModeType = "gaussian"
+DEFAULT_PAD_VALUE: int = 255
+DEFAULT_BATCH_SIZE: int = 1
+DEFAULT_ACTIVATION: ActivationType = None
+DEFAULT_THRESHOLD: float = 0.5
