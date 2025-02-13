f1156f3b05615a8d3da48866ea357d82d53804b6
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 04:15:46 +0000

    Add package learning-assistant/4.9.0/learning_assistant-4.9.0.tar.gz

---
+++ packages/learning-assistant/learning_assistant-4.9.0.tar.gz/learning_assistant-4.9.0/learning_assistant/__init__.py
@@ -0,0 +1,7 @@
+"""
+Plugin for a learning assistant backend, intended for use within edx-platform.
+"""
+
+__version__ = '4.9.0'
+
+default_app_config = 'learning_assistant.apps.LearningAssistantConfig'  # pylint: disable=invalid-name
