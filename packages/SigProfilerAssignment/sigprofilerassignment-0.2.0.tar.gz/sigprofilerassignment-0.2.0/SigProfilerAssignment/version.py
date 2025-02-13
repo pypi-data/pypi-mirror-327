202d2dee41693884d213eb83d0ca673669fd6fc9
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 06:05:20 +0000

    Add package SigProfilerAssignment/0.2.0/sigprofilerassignment-0.2.0.tar.gz

---
+++ packages/SigProfilerAssignment/sigprofilerassignment-0.2.0.tar.gz/sigprofilerassignment-0.2.0/SigProfilerAssignment/version.py
@@ -0,0 +1,8 @@
+
+# THIS FILE IS GENERATED FROM SigProfilerAssignment SETUP.PY
+short_version = '0.2.0'
+version = '0.2.0'
+Update = 'v0.2.0: Require Pandas and Numpy >= 2.0.0 and Python >= 3.9'
+
+    
+    
