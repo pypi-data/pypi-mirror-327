8ba206eda7431b736dbd0de17ccfac5eefb3b3fa
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 05:05:01 +0000

    Add package structifyai/1.93.0/structifyai-1.93.0-py3-none-any.whl

---
+++ packages/structifyai/structifyai-1.93.0-py3-none-any.whl/structify/types/admin/training_dataset_size_response.py
@@ -0,0 +1,29 @@
+# File generated from our OpenAPI spec by Stainless. See CONTRIBUTING.md for details.
+
+from typing import List
+from typing_extensions import Literal, TypeAlias
+
+from ..._models import BaseModel
+
+__all__ = ["TrainingDatasetSizeResponse", "TrainingDatasetSizeResponseItem"]
+
+
+class TrainingDatasetSizeResponseItem(BaseModel):
+    count: int
+
+    name: str
+
+    status: Literal[
+        "Unlabeled",
+        "NavLabeled",
+        "SaveLabeled",
+        "NavVerified",
+        "SaveVerified",
+        "Pending",
+        "Skipped",
+        "SuspiciousNav",
+        "SuspiciousSave",
+    ]
+
+
+TrainingDatasetSizeResponse: TypeAlias = List[TrainingDatasetSizeResponseItem]
