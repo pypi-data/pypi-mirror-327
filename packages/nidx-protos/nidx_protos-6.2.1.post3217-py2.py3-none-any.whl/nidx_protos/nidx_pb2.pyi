daf0bc4ba0a3c1a1432e062588b118f0778ec3f0
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 03:16:28 +0000

    Add package nidx-protos/6.2.1.post3217/nidx_protos-6.2.1.post3217-py2.py3-none-any.whl

---
+++ packages/nidx-protos/nidx_protos-6.2.1.post3217-py2.py3-none-any.whl/nidx_protos/nidx_pb2.pyi
@@ -0,0 +1,9 @@
+from nucliadb_protos import nodereader_pb2 as _nodereader_pb2
+from nucliadb_protos import noderesources_pb2 as _noderesources_pb2
+from nucliadb_protos import utils_pb2 as _utils_pb2
+from nucliadb_protos import nodewriter_pb2 as _nodewriter_pb2
+from nucliadb_protos import noderesources_pb2 as _noderesources_pb2_1
+from google.protobuf import descriptor as _descriptor
+from typing import ClassVar as _ClassVar
+
+DESCRIPTOR: _descriptor.FileDescriptor
