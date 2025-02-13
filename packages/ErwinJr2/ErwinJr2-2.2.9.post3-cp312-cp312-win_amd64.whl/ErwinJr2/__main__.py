98901e810ea816634a9167dc92204cdd4dd00515
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 14:14:52 +0000

    Add package ErwinJr2/2.2.9.post3/ErwinJr2-2.2.9.post3-cp312-cp312-win_amd64.whl

---
+++ packages/ErwinJr2/ErwinJr2-2.2.9.post3-cp312-cp312-win_amd64.whl/ErwinJr2/__main__.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+# -*- coding:utf-8 -*-
+"""
+Make `python -m ErwinJr` an alias for running `ErwinJr`.
+"""
+
+import os
+import sys
+
+from ErwinJr2.ErwinJr import main
+
+
+def __main__():
+    try:
+        fileName = os.path.abspath(sys.argv[1])
+    except IndexError:
+        fileName = None
+    main(fileName)
+
+
+if __name__ == "__main__":
+    __main__()
