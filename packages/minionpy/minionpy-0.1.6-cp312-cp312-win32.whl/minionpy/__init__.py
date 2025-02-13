04c147dec2f7317becf9447c11c053c8ede9cdd2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 04:38:19 +0000

    Add package minionpy/0.1.6/minionpy-0.1.6-cp312-cp312-win32.whl

---
+++ packages/minionpy/minionpy-0.1.6-cp312-cp312-win32.whl/minionpy/__init__.py
@@ -0,0 +1,12 @@
+from .algorithms import *
+from .test_functions import *
+from .cec import *
+
+try : from .cec_2011 import CEC2011
+except : pass
+
+# Define the package's version
+__version__ = "0.1.6"
+
+# Optionally, define the package name
+__name__ = "minionpy"
