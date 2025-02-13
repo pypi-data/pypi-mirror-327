d14c441890ef647612e51ffa787913e21e142852
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 05:05:26 +0000

    Add package femtum-sdk/0.11.7/femtum_sdk-0.11.7-py3-none-any.whl

---
+++ packages/femtum-sdk/femtum_sdk-0.11.7-py3-none-any.whl/femtum_sdk/core/die_pb2.pyi
@@ -0,0 +1,71 @@
+import femtum_sdk.core.tag_pb2 as _tag_pb2
+from google.protobuf.internal import containers as _containers
+from google.protobuf import descriptor as _descriptor
+from google.protobuf import message as _message
+from typing import ClassVar as _ClassVar, Iterable as _Iterable, Mapping as _Mapping, Optional as _Optional, Union as _Union
+
+DESCRIPTOR: _descriptor.FileDescriptor
+
+class Die(_message.Message):
+    __slots__ = ("Id", "CreatedAt", "UpdatedAt", "Name", "Tags")
+    ID_FIELD_NUMBER: _ClassVar[int]
+    CREATEDAT_FIELD_NUMBER: _ClassVar[int]
+    UPDATEDAT_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    TAGS_FIELD_NUMBER: _ClassVar[int]
+    Id: str
+    CreatedAt: str
+    UpdatedAt: str
+    Name: str
+    Tags: _containers.RepeatedCompositeFieldContainer[_tag_pb2.Tag]
+    def __init__(self, Id: _Optional[str] = ..., CreatedAt: _Optional[str] = ..., UpdatedAt: _Optional[str] = ..., Name: _Optional[str] = ..., Tags: _Optional[_Iterable[_Union[_tag_pb2.Tag, _Mapping]]] = ...) -> None: ...
+
+class UpdateDieRequest(_message.Message):
+    __slots__ = ("Id", "Name")
+    ID_FIELD_NUMBER: _ClassVar[int]
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    Id: str
+    Name: str
+    def __init__(self, Id: _Optional[str] = ..., Name: _Optional[str] = ...) -> None: ...
+
+class DiesFilterRequest(_message.Message):
+    __slots__ = ("Name", "Tags")
+    NAME_FIELD_NUMBER: _ClassVar[int]
+    TAGS_FIELD_NUMBER: _ClassVar[int]
+    Name: str
+    Tags: _containers.RepeatedCompositeFieldContainer[_tag_pb2.Tag]
+    def __init__(self, Name: _Optional[str] = ..., Tags: _Optional[_Iterable[_Union[_tag_pb2.Tag, _Mapping]]] = ...) -> None: ...
+
+class ListByPageDiesRequest(_message.Message):
+    __slots__ = ("PageNumber", "PageSize", "Filters")
+    PAGENUMBER_FIELD_NUMBER: _ClassVar[int]
+    PAGESIZE_FIELD_NUMBER: _ClassVar[int]
+    FILTERS_FIELD_NUMBER: _ClassVar[int]
+    PageNumber: int
+    PageSize: int
+    Filters: DiesFilterRequest
+    def __init__(self, PageNumber: _Optional[int] = ..., PageSize: _Optional[int] = ..., Filters: _Optional[_Union[DiesFilterRequest, _Mapping]] = ...) -> None: ...
+
+class DiesPage(_message.Message):
+    __slots__ = ("Items", "Total", "Index", "Size")
+    ITEMS_FIELD_NUMBER: _ClassVar[int]
+    TOTAL_FIELD_NUMBER: _ClassVar[int]
+    INDEX_FIELD_NUMBER: _ClassVar[int]
+    SIZE_FIELD_NUMBER: _ClassVar[int]
+    Items: _containers.RepeatedCompositeFieldContainer[Die]
+    Total: int
+    Index: int
+    Size: int
+    def __init__(self, Items: _Optional[_Iterable[_Union[Die, _Mapping]]] = ..., Total: _Optional[int] = ..., Index: _Optional[int] = ..., Size: _Optional[int] = ...) -> None: ...
+
+class FindDieByIdRequest(_message.Message):
+    __slots__ = ("Id",)
+    ID_FIELD_NUMBER: _ClassVar[int]
+    Id: str
+    def __init__(self, Id: _Optional[str] = ...) -> None: ...
+
+class OptionalDie(_message.Message):
+    __slots__ = ("Die",)
+    DIE_FIELD_NUMBER: _ClassVar[int]
+    Die: Die
+    def __init__(self, Die: _Optional[_Union[Die, _Mapping]] = ...) -> None: ...
