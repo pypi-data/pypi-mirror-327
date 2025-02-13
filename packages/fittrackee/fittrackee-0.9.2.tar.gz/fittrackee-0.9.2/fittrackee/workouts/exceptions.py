9931f4acc6d79810a3f53b67dd92a565e20e157d
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 03:26:44 +0000

    Add package fittrackee/0.9.2/fittrackee-0.9.2.tar.gz

---
+++ packages/fittrackee/fittrackee-0.9.2.tar.gz/fittrackee-0.9.2/fittrackee/workouts/exceptions.py
@@ -0,0 +1,18 @@
+from fittrackee.exceptions import GenericException
+
+
+class InvalidGPXException(GenericException):
+    pass
+
+
+class WorkoutException(GenericException):
+    pass
+
+
+class WorkoutGPXException(GenericException):
+    pass
+
+
+class WorkoutForbiddenException(GenericException):
+    def __init__(self) -> None:
+        super().__init__("error", "you do not have permissions")
