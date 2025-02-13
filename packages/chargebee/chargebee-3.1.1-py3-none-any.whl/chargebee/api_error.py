dae13de88c37fbe7b4fc357efd6d35688e15fc73
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 03:26:34 +0000

    Add package chargebee/3.1.1/chargebee-3.1.1-py3-none-any.whl

---
+++ packages/chargebee/chargebee-3.1.1-py3-none-any.whl/chargebee/api_error.py
@@ -0,0 +1,29 @@
+class APIError(Exception):
+
+    def __init__(self, http_code, json_obj):
+        Exception.__init__(self, json_obj.get("message"))
+        self.json_obj = json_obj
+        self.http_status_code = http_code
+        self.type = json_obj.get("type")
+        self.api_error_code = json_obj.get("api_error_code")
+        self.param = json_obj.get("param")
+
+        self.error_code = json_obj["error_code"]
+        self.error_cause = json_obj.get("error_cause", None)
+        self.http_code = http_code
+        self.http_body = None
+
+
+class PaymentError(APIError):
+    def __init__(self, http_code, json_obj):
+        APIError.__init__(self, http_code, json_obj)
+
+
+class InvalidRequestError(APIError):
+    def __init__(self, http_code, json_obj):
+        APIError.__init__(self, http_code, json_obj)
+
+
+class OperationFailedError(APIError):
+    def __init__(self, http_code, json_obj):
+        APIError.__init__(self, http_code, json_obj)
