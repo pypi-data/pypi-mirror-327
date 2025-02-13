d4cf5340d2ecbba2871ef632dfbd10a587ca24cd
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-02-13 03:26:39 +0000

    Add package crawlee/0.6.0b1/crawlee-0.6.0b1-py3-none-any.whl

---
+++ packages/crawlee/crawlee-0.6.0b1-py3-none-any.whl/crawlee/browsers/__init__.py
@@ -0,0 +1,11 @@
+try:
+    from ._browser_pool import BrowserPool
+    from ._playwright_browser_controller import PlaywrightBrowserController
+    from ._playwright_browser_plugin import PlaywrightBrowserPlugin
+except ImportError as exc:
+    raise ImportError(
+        "To import this, you need to install the 'playwright' extra. "
+        "For example, if you use pip, run `pip install 'crawlee[playwright]'`.",
+    ) from exc
+
+__all__ = ['BrowserPool', 'PlaywrightBrowserController', 'PlaywrightBrowserPlugin']
