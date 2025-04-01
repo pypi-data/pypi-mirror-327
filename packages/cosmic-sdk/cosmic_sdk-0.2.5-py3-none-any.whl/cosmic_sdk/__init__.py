27d236cd189e60e86ef4e972b874ee72a64e1963
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:27:16 +0000

    Add package cosmic-sdk/0.2.5/cosmic_sdk-0.2.5-py3-none-any.whl

---
+++ packages/cosmic-sdk/cosmic_sdk-0.2.5-py3-none-any.whl/cosmic_sdk/__init__.py
@@ -0,0 +1,11 @@
+from cosmic_sdk.connectors import PostHogService
+from cosmic_sdk.secrets import SecretsManager
+from cosmic_sdk.connectors import SlackService
+
+__version__ = "0.0.1"
+
+__all__ = [
+    "PostHogService",
+    "SecretsManager",
+    "SlackService"
+]
