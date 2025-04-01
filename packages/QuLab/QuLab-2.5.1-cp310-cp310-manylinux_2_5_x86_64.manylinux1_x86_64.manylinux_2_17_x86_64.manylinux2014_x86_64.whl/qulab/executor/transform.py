ac3e68cd318cedd8e348233d26d97831dde7d0cf
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:33:00 +0000

    Add package QuLab/2.5.1/QuLab-2.5.1-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl

---
+++ packages/QuLab/QuLab-2.5.1-cp310-cp310-manylinux_2_5_x86_64.manylinux1_x86_64.manylinux_2_17_x86_64.manylinux2014_x86_64.whl/qulab/executor/transform.py
@@ -0,0 +1,55 @@
+from .storage import Result
+
+
+def _query_config(name: str, default=None):
+    import pickle
+
+    try:
+        with open('parameters.pkl', 'rb') as f:
+            parameters = pickle.load(f)
+    except:
+        parameters = {}
+
+    return parameters.get(name, default)
+
+
+def _update_config(updates):
+    import pickle
+
+    try:
+        with open('parameters.pkl', 'rb') as f:
+            parameters = pickle.load(f)
+    except:
+        parameters = {}
+
+    for k, v in updates.items():
+        parameters[k] = v
+
+    with open('parameters.pkl', 'wb') as f:
+        pickle.dump(parameters, f)
+
+
+def update_parameters(result: Result):
+    update_config(result.parameters)
+
+
+query_config = _query_config
+update_config = _update_config
+
+
+def set_config_api(query_method, update_method):
+    """
+    Set the query and update methods for the config.
+
+    Args:
+        query_method: The query method.
+            the method should take a key and return the value.
+        update_method: The update method.
+            the method should take a dict of updates.
+    """
+    global query_config, update_config
+
+    query_config = query_method
+    update_config = update_method
+
+    return query_config, update_config
