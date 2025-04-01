d3026543f07141ec40ed0f5e4bc623bb6532cdbe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:31:24 +0000

    Add package evalscope/0.11.0/evalscope-0.11.0.tar.gz

---
+++ packages/evalscope/evalscope-0.11.0.tar.gz/evalscope-0.11.0/evalscope/utils/model_utils.py
@@ -0,0 +1,24 @@
+from enum import Enum
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from transformers import GenerationConfig
+
+
+class EvalBackend(Enum):
+    #  NOTE: compatible with ms-swfit v2.x
+    NATIVE = 'Native'
+    OPEN_COMPASS = 'OpenCompass'
+    VLM_EVAL_KIT = 'VLMEvalKit'
+    RAG_EVAL = 'RAGEval'
+    THIRD_PARTY = 'ThirdParty'
+
+
+def fix_do_sample_warning(generation_config: 'GenerationConfig') -> None:
+    # Use the default values of temperature/top_p/top_k in generation_config.
+    if generation_config.temperature == 0:
+        generation_config.do_sample = False
+    if generation_config.do_sample is False:
+        generation_config.temperature = 1.
+        generation_config.top_p = 1.
+        generation_config.top_k = 50
