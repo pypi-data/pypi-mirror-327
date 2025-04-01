e59fb7f86331c9bcc107ff97c2ae7d7cfa19c8d4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:35:54 +0000

    Add package mtxai/0.0.166/mtxai-0.0.166.tar.gz

---
+++ packages/mtxai/mtxai-0.0.166.tar.gz/mtxai-0.0.166/mtmai/agents/webui_agent.py
@@ -0,0 +1,29 @@
+import logging
+from typing import Awaitable, Callable, List
+from autogen_core import  MessageContext, RoutedAgent, default_subscription, message_handler
+from autogen_core.models import (
+    AssistantMessage,
+    ChatCompletionClient,
+    LLMMessage,
+    SystemMessage,
+    UserMessage,
+)
+from mtmaisdk.clients.rest.models.chat_message import ChatMessage
+from mtmaisdk.clients.rest_client import AsyncRestApi
+from rich.console import Console
+from rich.markdown import Markdown
+
+logger = logging.getLogger(__name__)
+@default_subscription
+class UIAgent(RoutedAgent):
+    """Handles UI-related tasks and message processing for the distributed group chat system."""
+
+    def __init__(self, gomtmapi: AsyncRestApi) -> None:
+        super().__init__("UI Agent")
+        self.gomtmapi = gomtmapi
+
+    @message_handler
+    async def handle_message_chunk(self, message: ChatMessage, ctx: MessageContext) -> None:
+        logger.info(f"UI Agent 收到消息: {message}")
+
+        await self.gomtmapi.teams_api.team_get(tenant=message.tenant_id, team=message.team_id)
