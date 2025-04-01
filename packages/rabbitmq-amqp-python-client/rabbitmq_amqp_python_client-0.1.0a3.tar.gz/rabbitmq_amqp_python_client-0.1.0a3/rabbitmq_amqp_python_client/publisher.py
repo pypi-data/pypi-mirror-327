c5bdb66ee18a4d38b761d9e44fb95ca423a17177
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:09:52 +0000

    Add package rabbitmq-amqp-python-client/0.1.0a3/rabbitmq_amqp_python_client-0.1.0a3.tar.gz

---
+++ packages/rabbitmq-amqp-python-client/rabbitmq_amqp_python_client-0.1.0a3.tar.gz/rabbitmq_amqp_python_client-0.1.0a3/rabbitmq_amqp_python_client/publisher.py
@@ -0,0 +1,37 @@
+import logging
+from typing import Optional
+
+from .options import SenderOptionUnseattle
+from .qpid.proton._delivery import Delivery
+from .qpid.proton._message import Message
+from .qpid.proton.utils import (
+    BlockingConnection,
+    BlockingSender,
+)
+
+logger = logging.getLogger(__name__)
+
+
+class Publisher:
+    def __init__(self, conn: BlockingConnection, addr: str):
+        self._sender: Optional[BlockingSender] = None
+        self._conn = conn
+        self._addr = addr
+        self._open()
+
+    def _open(self) -> None:
+        if self._sender is None:
+            logger.debug("Creating Sender")
+            self._sender = self._create_sender(self._addr)
+
+    def publish(self, message: Message) -> Delivery:
+        if self._sender is not None:
+            return self._sender.send(message)
+
+    def close(self) -> None:
+        logger.debug("Closing Sender")
+        if self._sender is not None:
+            self._sender.close()
+
+    def _create_sender(self, addr: str) -> BlockingSender:
+        return self._conn.create_sender(addr, options=SenderOptionUnseattle(addr))
