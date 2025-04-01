51132c482f65e9843a0703f719d0ffa2ad4c68ba
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:31:39 +0000

    Add package cosmic-sdk/0.3.3/cosmic_sdk-0.3.3.tar.gz

---
+++ packages/cosmic-sdk/cosmic_sdk-0.3.3.tar.gz/cosmic_sdk-0.3.3/src/cosmic_sdk/connectors/slack/service.py
@@ -0,0 +1,61 @@
+import logging
+#from cosmic_sdk.connectors.posthog.models import Group, User, Event
+import requests
+import sys
+import os
+#from .models import SlackCredentials, SlackMessage, SlackHistoryRequest, SlackMessageResponse, SlackHistoryResponse
+from slack_sdk import WebClient
+from slack_sdk.errors import SlackApiError
+
+# Ref: https://tools.slack.dev/python-slack-sdk/api-docs/slack_sdk/
+logger = logging.getLogger(__name__)
+
+class SlackService:
+    def __init__(self, credentials: dict):
+        print(credentials)
+        print(credentials.get('slack_bot_token'))
+        self.slack_bot_token = credentials.get('slack_bot_token')
+        if not self.slack_bot_token:
+            raise ValueError("Missing required credential: 'slack_bot_token'")
+        self.client = WebClient(token=self.slack_bot_token)
+        self.channel_id = ''
+        
+        
+
+    def set_channel_id(self, channel_id: str):
+        self.channel_id = channel_id
+
+    def get_channel_id(self):
+        return self.channel_id
+
+
+    def get_token(self):
+        return self.client.token
+    
+    
+    def send_message(self, message):
+        try:
+            response = self.client.chat_postMessage(
+                channel=message.channel_id, 
+                text=message.text
+            )
+            return response
+        except SlackApiError as e:
+            raise ValueError(f"Failed to send message: {str(e)}")
+        
+    def get_channel_history(self, request):
+        try:
+            response = self.client.conversations_history(
+                channel=request.channel,
+                limit=request.limit,
+                oldest=request.oldest,
+                latest=request.latest,
+                inclusive=request.inclusive
+            )
+            return response
+        except SlackApiError as e:
+            raise ValueError(f"Failed to get channel history: {str(e)}")
+        
+
+
+    
