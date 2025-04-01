8d0b01a0459a6a4a99756b4419f4a757c03670a4
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:13:05 +0000

    Add package eurelis-llmatoolkit/2.0.0.dev8/eurelis_llmatoolkit-2.0.0.dev8.tar.gz

---
+++ packages/eurelis-llmatoolkit/eurelis_llmatoolkit-2.0.0.dev8.tar.gz/eurelis_llmatoolkit-2.0.0.dev8/src/eurelis_llmatoolkit/llamaindex/readers/abstract_reader_adapter.py
@@ -0,0 +1,17 @@
+from abc import ABC, abstractmethod
+
+
+class AbstractReaderAdapter(ABC):
+    required_params = []
+
+    def __init__(self, config):
+        self.config = config
+        self.reader = None
+
+    @abstractmethod
+    def load_data(self, *args, **kwargs):
+        """Méthode abstraite que chaque sous-classe doit implémenter pour appeler la méthode de chargement des données du reader"""
+
+    def _get_load_data_params(self):
+        """Récupère les paramètres nécessaires pour charger les données à partir de la configuration."""
+        return {param: self.config[param] for param in self.__class__.required_params}
