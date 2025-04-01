8fc7d605cb7beab88e773370cc7edf94909aaf1a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:10:25 +0000

    Add package gmk-utils/0.1.1/gmk_utils-0.1.1-py3-none-any.whl

---
+++ packages/gmk-utils/gmk_utils-0.1.1-py3-none-any.whl/gmk_utils/functions.py
@@ -0,0 +1,7 @@
+def add(a: int, b: int) -> int:
+    """Add two integers."""
+    return a + b
+
+def subtract(a: int, b: int) -> int:
+    """Subtract one integer from another."""
+    return a - b
