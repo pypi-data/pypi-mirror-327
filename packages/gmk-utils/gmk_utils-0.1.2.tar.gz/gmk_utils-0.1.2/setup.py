cc0da7d76fdfaf90c3bbf503f059ad019291c3f2
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:06:44 +0000

    Add package gmk-utils/0.1.2/gmk_utils-0.1.2.tar.gz

---
+++ packages/gmk-utils/gmk_utils-0.1.2.tar.gz/gmk_utils-0.1.2/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open("README.md", "r") as fh:
+    long_description = fh.read()
+
+setup(
+    name="gmk_utils",
+    version="0.1.2",
+    packages=find_packages(where="src"),
+    package_dir={"": "src"},
+    install_requires=[
+        "numpy>=1.21.0",
+    ],
+    data_files=[
+        ("gmk_utils/solrs", ["Z:/pypi/gmk_utils/src/gmk_utils/solr/solr-8.9.0.zip"])
+    ],
+    tests_require=["pytest"],
+    test_suite="tests/",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    url="https://github.com/gmk-dev/gmk_utils",
+    license="MIT",
+)
