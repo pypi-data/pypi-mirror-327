4b861357259386ea15294d77f662ba6fca4f9519
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:09:38 +0000

    Add package fmu-sumo/2.3.1/fmu_sumo-2.3.1-py3-none-any.whl

---
+++ packages/fmu-sumo/fmu_sumo-2.3.1-py3-none-any.whl/fmu/sumo/explorer/objects/dictionary.py
@@ -0,0 +1,30 @@
+"""Module containing class for dictionary object"""
+
+import json
+from typing import Dict
+
+from sumo.wrapper import SumoClient
+
+from fmu.sumo.explorer.objects._child import Child
+
+
+class Dictionary(Child):
+    """Class representig a dictionary object in Sumo"""
+
+    _parsed: dict
+
+    def __init__(self, sumo: SumoClient, metadata: Dict, blob=None) -> None:
+        """
+        Args:
+            sumo (SumoClient): connection to Sumo
+            metadata (dict): dictionary metadata
+        """
+        self._parsed = None
+
+        super().__init__(sumo, metadata, blob)
+
+    def parse(self) -> Dict:
+        if self._parsed is None:
+            self._parsed = json.loads(self.blob.decode("utf-8"))
+
+        return self._parsed
