53dc8e3a54dd280176dacde3b5d47ee17b975f74
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:33:55 +0000

    Add package jsonbp/1.2.0/jsonbp-1.2.0-py3-none-any.whl

---
+++ packages/jsonbp/jsonbp-1.2.0-py3-none-any.whl/jsonbp/types/bool.py
@@ -0,0 +1,53 @@
+
+import jsonbp
+
+_defaults = {
+	'coerce': True
+}
+
+
+def _format(value, specs):
+	return "true" if value else "false"
+
+
+def _parse(value, specs):
+	if isinstance(value, jsonbp.unquoted_str):
+		if value in ('true', 'false'):
+			return True, value == "true"
+
+	if not specs['coerce']:
+		return False, {
+			"error": jsonbp.ErrorType.VALUE_PARSING,
+			"context": {}
+		}
+
+	# coercion attempts
+	# check if it's 'null' or empty string
+
+	if None == value or 0 == len(value):
+		return True, False
+
+	try:
+		rawValue = float(value)
+		# check if is effectively zero or NaN
+		if 0 == rawValue or rawValue != rawValue:
+			return True, False
+
+	except Exception as e:
+		# just an attempt, no problem
+		# swallow this exception
+		pass
+
+	# if none of the above, then most likely
+	# it's a truthy value
+
+	return True, True
+
+
+type_specs = {
+	'name': 'Bool',
+	'parser': _parse,
+	'formatter': _format,
+	'defaults': _defaults
+}
+
---
+++ packages/jsonbp/jsonbp-1.2.0-py3-none-any.whl/jsonbp/types/instant.py
@@ -0,0 +1,40 @@
+
+from datetime import datetime
+
+_defaults = {
+	'iso': True,
+	'isoResolution': 'milliseconds',
+	'format': "%Y-%m-%dT%H:%M:%S%z"
+}
+
+# allowed_resolutions:
+# https://docs.python.org/3/library/datetime.html#datetime.datetime.isoformat
+
+def _format(value, specs):
+	if specs['iso']:
+		resolution = specs['isoResolution']
+		result = value.isoformat(timespec=resolution)
+		return f'"{result}"'
+
+	result = value.strftime(specs['format'])
+	return f'"{result}"'
+
+
+def _parse(value, specs):
+	sanedValue = str(value)
+
+	parsed_date = (
+		datetime.fromisoformat(sanedValue) if specs['iso']
+		else datetime.strptime(sanedValue, specs['format'])
+	)
+
+	return True, parsed_date
+
+
+type_specs = {
+	'name': 'Instant',
+	'parser': _parse,
+	'formatter': _format,
+	'defaults': _defaults
+}
+
