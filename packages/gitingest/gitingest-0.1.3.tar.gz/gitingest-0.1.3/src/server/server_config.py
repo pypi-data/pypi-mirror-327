32e53c0cb95bb6041df1701faa590c151f37053b
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:15:43 +0000

    Add package gitingest/0.1.3/gitingest-0.1.3.tar.gz

---
+++ packages/gitingest/gitingest-0.1.3.tar.gz/gitingest-0.1.3/src/server/server_config.py
@@ -0,0 +1,17 @@
+""" Configuration for the server. """
+
+from fastapi.templating import Jinja2Templates
+
+MAX_DISPLAY_SIZE: int = 300_000
+DELETE_REPO_AFTER: int = 60 * 60  # In seconds
+
+
+EXAMPLE_REPOS: list[dict[str, str]] = [
+    {"name": "Gitingest", "url": "https://github.com/cyclotruc/gitingest"},
+    {"name": "FastAPI", "url": "https://github.com/tiangolo/fastapi"},
+    {"name": "Flask", "url": "https://github.com/pallets/flask"},
+    {"name": "Excalidraw", "url": "https://github.com/excalidraw/excalidraw"},
+    {"name": "ApiAnalytics", "url": "https://github.com/tom-draper/api-analytics"},
+]
+
+templates = Jinja2Templates(directory="server/templates")
