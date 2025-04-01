4693bef36ff59af46cfa511867067470ab02aa20
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:16:39 +0000

    Add package plurally/0.3.42/plurally-0.3.42-py3-none-any.whl

---
+++ packages/plurally/plurally-0.3.42-py3-none-any.whl/plurally/models/sf/task.py
@@ -0,0 +1,16 @@
+import enum
+
+
+DEFAULT_PROPERTIES = "Subject,Status,Priority,Description,ActivityDate"
+REQUIRED_PROPERTIES = ("Subject", "Status", "ActivityDate")
+
+
+def query_statuses(service):
+    return service.query("SELECT MasterLabel FROM TaskStatus")
+
+
+def get_TaskStatuses(service):
+    return enum.Enum(
+        "TaskStatus",
+        [status["MasterLabel"] for status in query_statuses(service)["records"]],
+    )
