c9b18cf4a4b982f0bee89bd4f05280cc73a77d5a
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:05:45 +0000

    Add package autoblock/1.3.0/autoblock-1.3.0.tar.gz

---
+++ packages/autoblock/autoblock-1.3.0.tar.gz/autoblock-1.3.0/setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+import os
+
+# Read long description from README.md
+with open("README.md", "r", encoding="utf-8") as fh:
+    long_description = fh.read()
+
+setup(
+    name="autoblock",
+    version="1.3.0",
+    packages=find_packages(include=["autoblock", "autoblock.*"]),  # Ensure `autoblock` package detected
+    description="Auto password changer for Django projects",
+    long_description=long_description,
+    long_description_content_type="text/markdown",
+    author="Parth",
+    author_email="parth@kanhasoft.com",
+    url="https://github.com/parth-kanhasoft/autoblock",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.10",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+    ],
+    install_requires=[
+        "Django>=4.0",
+    ],
+    python_requires=">=3.10",
+)
