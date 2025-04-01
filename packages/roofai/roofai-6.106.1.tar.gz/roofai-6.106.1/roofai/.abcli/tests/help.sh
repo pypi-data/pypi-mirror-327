66fad203e58bf9939842d2d98254d96937051c57
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:18:38 +0000

    Add package roofai/6.106.1/roofai-6.106.1.tar.gz

---
+++ packages/roofai/roofai-6.106.1.tar.gz/roofai-6.106.1/roofai/.abcli/tests/help.sh
@@ -0,0 +1,30 @@
+#! /usr/bin/env bash
+
+function test_roofai_help() {
+    local options=$1
+
+    local module
+    for module in \
+        "@gmaps" \
+        "@google_maps" \
+        "@google_maps get_static_image" \
+        "@google_maps geocode" \
+        \
+        "roofai dataset" \
+        "roofai dataset ingest" \
+        "roofai dataset review" \
+        \
+        "roofai semseg" \
+        "roofai semseg predict" \
+        "roofai semseg train" \
+        \
+        "roofai"; do
+        abcli_eval ,$options \
+            abcli_help $module
+        [[ $? -ne 0 ]] && return 1
+
+        abcli_hr
+    done
+
+    return 0
+}
---
+++ packages/roofai/roofai-6.106.1.tar.gz/roofai-6.106.1/setup.py
@@ -0,0 +1,29 @@
+from roofai import NAME, VERSION, DESCRIPTION, REPO_NAME
+from blueness.pypi import setup
+
+
+setup(
+    filename=__file__,
+    repo_name=REPO_NAME,
+    name=NAME,
+    version=VERSION,
+    description=DESCRIPTION,
+    packages=[
+        NAME,
+        f"{NAME}.dataset",
+        f"{NAME}.dataset.ingest",
+        f"{NAME}.google_maps",
+        f"{NAME}.help",
+        f"{NAME}.help.google_maps",
+        f"{NAME}.semseg",
+        f"{NAME}.tests",
+    ],
+    include_package_data=True,
+    package_data={
+        NAME: [
+            "config.env",
+            "sample.env",
+            ".abcli/**/*.sh",
+        ],
+    },
+)
