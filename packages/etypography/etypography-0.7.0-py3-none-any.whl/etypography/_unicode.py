e881620f562aad222fc6b1a20d322bb090c09744
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:36:23 +0000

    Add package etypography/0.7.0/etypography-0.7.0-py3-none-any.whl

---
+++ packages/etypography/etypography-0.7.0-py3-none-any.whl/etypography/_unicode.py
@@ -0,0 +1,11 @@
+from __future__ import annotations
+
+__all__ = ["character_is_normally_rendered"]
+
+# python
+from unicodedata import category as unicode_category
+
+
+def character_is_normally_rendered(character: str) -> bool:
+    # http://www.unicode.org/reports/tr44/#GC_Values_Table
+    return unicode_category(character)[0] not in "CZ"
