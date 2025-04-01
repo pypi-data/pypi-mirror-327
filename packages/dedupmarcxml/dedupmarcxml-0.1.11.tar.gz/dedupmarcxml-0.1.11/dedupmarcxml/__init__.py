ec26293e3ae921e693b7c60a6e8382d05232a7ef
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:13:30 +0000

    Add package dedupmarcxml/0.1.11/dedupmarcxml-0.1.11.tar.gz

---
+++ packages/dedupmarcxml/dedupmarcxml-0.1.11.tar.gz/dedupmarcxml-0.1.11/dedupmarcxml/__init__.py
@@ -0,0 +1,3 @@
+from .briefrecord import XmlBriefRec, JsonBriefRec, XmlBriefRecFactory, JsonBriefRecFactory
+from .evaluate import evaluate_records_similarity, get_similarity_score
+from .version import __version__, commit_message
