2cdb98db3418b921453e2169e8071d59dec10057
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:16:39 +0000

    Add package mcsl-lib/1.3.post3/mcsl_lib-1.3.post3.tar.gz

---
+++ packages/mcsl-lib/mcsl_lib-1.3.post3.tar.gz/mcsl_lib-1.3.post3/setup.py
@@ -0,0 +1,23 @@
+from setuptools import setup, find_packages
+
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
+setup(
+    name="mcsl-lib",
+    version="1.3.post3",
+    packages=find_packages(),
+    install_requires=requirements,
+    description="A developing powerful Minecraft library in Python to host a Minecraft server.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    author="Soumalya Das",
+    author_email="dassantu8385@gmail.com",
+    url="https://github.com/pro-grammer-SD/mcsl",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires='>=3.6',
+)
