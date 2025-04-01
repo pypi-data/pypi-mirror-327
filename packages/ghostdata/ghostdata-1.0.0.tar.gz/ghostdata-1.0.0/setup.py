3c052febe8172d388989e1b3b1186f783908a6c8
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:32:18 +0000

    Add package ghostdata/1.0.0/ghostdata-1.0.0.tar.gz

---
+++ packages/ghostdata/ghostdata-1.0.0.tar.gz/ghostdata-1.0.0/setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="ghostdata",  # The package name on PyPI
+    version="1.0.0",  # Start with version 1.0.0
+    packages=find_packages(),
+    install_requires=[
+        "pillow",
+        "pypdf",
+        "mutagen",
+        "imageio[ffmpeg]",
+        "ffmpeg-python",
+    ],
+    author="Oxde",
+    author_email="your-email@example.com",
+    description="GhostData: Securely remove metadata from images, PDFs, videos, and audio files.",
+    long_description=open("README.md").read(),
+    long_description_content_type="text/markdown",
+    url="https://github.com/oxde/ghostdata",
+    license="CC BY-NC 4.0",
+    classifiers=[
+        "Programming Language :: Python :: 3",
+        "License :: Other/Proprietary License",
+        "Operating System :: OS Independent",
+    ],
+    python_requires=">=3.6",
+)
