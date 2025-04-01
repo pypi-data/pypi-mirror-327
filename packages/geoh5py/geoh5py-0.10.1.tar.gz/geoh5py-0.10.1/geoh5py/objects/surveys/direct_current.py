48864edd468261a85952a1e6efb37b1ebbfa5985
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:30:47 +0000

    Add package geoh5py/0.10.1/geoh5py-0.10.1.tar.gz

---
+++ packages/geoh5py/geoh5py-0.10.1.tar.gz/geoh5py-0.10.1/geoh5py/objects/surveys/direct_current.py
@@ -0,0 +1,393 @@
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2025 Mira Geoscience Ltd.                                     '
+#                                                                              '
+#  This file is part of geoh5py.                                               '
+#                                                                              '
+#  geoh5py is free software: you can redistribute it and/or modify             '
+#  it under the terms of the GNU Lesser General Public License as published by '
+#  the Free Software Foundation, either version 3 of the License, or           '
+#  (at your option) any later version.                                         '
+#                                                                              '
+#  geoh5py is distributed in the hope that it will be useful,                  '
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              '
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               '
+#  GNU Lesser General Public License for more details.                         '
+#                                                                              '
+#  You should have received a copy of the GNU Lesser General Public License    '
+#  along with geoh5py.  If not, see <https://www.gnu.org/licenses/>.           '
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+# pylint: disable=too-many-ancestors
+
+from __future__ import annotations
+
+import logging
+import uuid
+from abc import ABC, abstractmethod
+from typing import cast
+
+import numpy as np
+
+from ...data import Data, ReferencedData, ReferenceValueMap
+from ...shared.utils import str_json_to_dict
+from ..curve import Curve
+
+
+logger = logging.getLogger(__name__)
+
+
+class BaseElectrode(Curve, ABC):
+    _potential_electrodes: PotentialElectrode | None = None
+    _current_electrodes: CurrentElectrode | None = None
+
+    def __init__(self, **kwargs):
+        self._ab_cell_id: ReferencedData | None = None
+
+        super().__init__(**kwargs)
+
+    @property
+    def ab_cell_id(self) -> ReferencedData | None:
+        """
+        Reference data entity mapping cells to a unique current dipole.
+        """
+        if getattr(self, "_ab_cell_id", None) is None:
+            child = self.get_data("A-B Cell ID")
+            if any(child) and isinstance(child[0], ReferencedData):
+                self.ab_cell_id = child[0]
+
+        if getattr(self, "_ab_cell_id", None) is not None:
+            return self._ab_cell_id
+
+        return None
+
+    @ab_cell_id.setter
+    def ab_cell_id(self, data: Data | np.ndarray):
+        if isinstance(data, Data):
+            if not isinstance(data, ReferencedData):
+                raise TypeError(f"ab_cell_id must be of type {ReferencedData}")
+
+            if data.parent.uid == self.uid:
+                self._ab_cell_id = data
+            else:
+                self._ab_cell_id = cast(ReferencedData, data.copy(parent=self))
+        else:
+            if data.dtype != np.int32:
+                logger.info("ab_cell_id values will be converted to type 'int32'")
+
+            if any(self.get_data("A-B Cell ID")):
+                child = self.get_data("A-B Cell ID")[0]
+                if isinstance(child, ReferencedData):
+                    child.values = data.astype(np.int32)
+            else:
+                complement: CurrentElectrode | PotentialElectrode = (
+                    self.current_electrodes
+                    if isinstance(self, PotentialElectrode)
+                    else self.potential_electrodes
+                )
+                attributes = {
+                    "values": data.astype(np.int32),
+                    "association": "CELL",
+                }
+                if complement is not None and complement.ab_cell_id is not None:
+                    attributes["entity_type"] = complement.ab_cell_id.entity_type
+                else:
+                    value_map = {ii: str(ii) for ii in range(data.max() + 1)}
+                    value_map[0] = "Unknown"
+                    attributes.update(
+                        {  # type: ignore
+                            "primitive_type": "REFERENCED",
+                            "value_map": value_map,
+                        }
+                    )
+
+                data = self.add_data({"A-B Cell ID": attributes})
+
+                if isinstance(data, ReferencedData):
+                    self._ab_cell_id = data
+
+    @property
+    def ab_map(self) -> ReferenceValueMap | None:
+        """
+        Get the ReferenceData.value_map of the ab_value_id
+        """
+        if isinstance(self.ab_cell_id, ReferencedData):
+            return self.ab_cell_id.value_map
+        return None
+
+    def copy(
+        self,
+        parent=None,
+        *,
+        copy_children: bool = True,
+        clear_cache: bool = False,
+        mask: np.ndarray | None = None,
+        cell_mask: np.ndarray | None = None,
+        **kwargs,
+    ):
+        """
+        Sub-class extension of :func:`~geoh5py.objects.cell_object.CellObject.copy`.
+        """
+        if parent is None:
+            parent = self.parent
+
+        omit_list = [
+            "_ab_cell_id",
+            "_metadata",
+            "_potential_electrodes",
+            "_current_electrodes",
+        ]
+        new_entity = super().copy(
+            parent=parent,
+            clear_cache=clear_cache,
+            copy_children=copy_children,
+            mask=mask,
+            cell_mask=cell_mask,
+            omit_list=omit_list,
+            **kwargs,
+        )
+
+        if self.cells is not None:
+            if mask is not None:
+                cell_mask = np.all(mask[self.cells], axis=1)
+            else:
+                cell_mask = np.ones(self.cells.shape[0], dtype=bool)
+
+            if self.ab_cell_id is not None and self.ab_cell_id.values is not None:
+                new_entity.ab_cell_id = self.ab_cell_id.values[cell_mask]
+
+        complement: CurrentElectrode | PotentialElectrode = (
+            self.current_electrodes
+            if isinstance(self, PotentialElectrode)
+            else self.potential_electrodes
+        )
+
+        # Set the mask of the complement
+        if (
+            new_entity.ab_cell_id is not None
+            and complement is not None
+            and complement.ab_cell_id is not None
+            and complement.ab_cell_id.values is not None
+            and complement.vertices is not None
+            and complement.cells is not None
+        ):
+            intersect = np.intersect1d(
+                new_entity.ab_cell_id.values,
+                complement.ab_cell_id.values,
+            )
+            cell_mask = np.r_[
+                [(val in intersect) for val in complement.ab_cell_id.values]
+            ]
+
+            # Convert cell indices to vertex indices
+            mask = np.zeros(complement.vertices.shape[0], dtype=bool)
+            mask[complement.cells[cell_mask, :]] = True
+
+            new_complement = super(Curve, complement).copy(  # type: ignore
+                parent=parent,
+                omit_list=omit_list,
+                copy_children=copy_children,
+                clear_cache=clear_cache,
+                mask=mask,
+                cell_mask=cell_mask,
+            )
+
+            if isinstance(self, PotentialElectrode):
+                new_entity.current_electrodes = new_complement
+            else:
+                new_entity.potential_electrodes = new_complement
+
+            if new_complement.ab_cell_id is None and complement.ab_cell_id is not None:
+                new_complement.ab_cell_id = complement.ab_cell_id.values[cell_mask]
+
+            # Re-number the ab_cell_id
+            value_map = {
+                val: ind
+                for ind, val in enumerate(
+                    np.r_[0, np.unique(new_entity.current_electrodes.ab_cell_id.values)]
+                )
+            }
+            new_map = {
+                val: dict(new_entity.current_electrodes.ab_cell_id.value_map.map)[val]
+                for val in value_map.values()
+            }
+            new_complement.ab_cell_id.values = np.asarray(
+                [value_map[val] for val in new_complement.ab_cell_id.values]
+            )
+            new_entity.ab_cell_id.values = np.asarray(
+                [value_map[val] for val in new_entity.ab_cell_id.values]
+            )
+            new_entity.ab_cell_id.entity_type.value_map = new_map
+
+        return new_entity
+
+    @property
+    @abstractmethod
+    def current_electrodes(self):
+        """
+        The associated current_electrodes (transmitters)
+        """
+
+    @staticmethod
+    def validate_metadata(value):
+        if isinstance(value, np.ndarray):
+            value = value[0]
+
+        if isinstance(value, bytes):
+            value = str_json_to_dict(value)
+
+        if isinstance(value, dict):
+            default_keys = ["Current Electrodes", "Potential Electrodes"]
+
+            # check if metadata has the required keys
+            if not all(key in value for key in default_keys):
+                raise ValueError(f"Input metadata must have for keys {default_keys}")
+
+        return value
+
+    @property
+    @abstractmethod
+    def potential_electrodes(self):
+        """
+        The associated potential_electrodes (receivers)
+        """
+
+
+class PotentialElectrode(BaseElectrode):
+    """
+    Ground potential electrode (receiver).
+    """
+
+    _TYPE_UID = uuid.UUID("{275ecee9-9c24-4378-bf94-65f3c5fbe163}")
+
+    @property
+    def current_electrodes(self):
+        """
+        The associated current electrode object (sources).
+        """
+        if getattr(self, "_current_electrodes", None) is None:
+            if self.metadata is not None and "Current Electrodes" in self.metadata:
+                transmitter = self.metadata["Current Electrodes"]
+                transmitter_entity = self.workspace.get_entity(transmitter)[0]
+
+                if isinstance(transmitter_entity, CurrentElectrode):
+                    self._current_electrodes = transmitter_entity
+
+        return self._current_electrodes
+
+    @current_electrodes.setter
+    def current_electrodes(self, current_electrodes: CurrentElectrode):
+        if not isinstance(current_electrodes, CurrentElectrode):
+            raise TypeError(
+                f"Provided current_electrodes must be of type {CurrentElectrode}. "
+                f"{type(current_electrodes)} provided."
+            )
+
+        metadata = {
+            "Current Electrodes": current_electrodes.uid,
+            "Potential Electrodes": self.uid,
+        }
+
+        self.metadata = metadata
+        current_electrodes.metadata = metadata
+
+        if isinstance(current_electrodes.ab_cell_id, ReferencedData) and isinstance(
+            self.ab_cell_id, ReferencedData
+        ):
+            self.ab_cell_id.entity_type = current_electrodes.ab_cell_id.entity_type
+
+    @property
+    def potential_electrodes(self):
+        """
+        The associated potential_electrodes (receivers)
+        """
+        return self
+
+
+class CurrentElectrode(BaseElectrode):
+    """
+    Ground direct current electrode (transmitter).
+    """
+
+    _TYPE_UID = uuid.UUID("{9b08bb5a-300c-48fe-9007-d206f971ea92}")
+
+    def __init__(self, **kwargs):
+        self._current_line_id: uuid.UUID | None = None
+
+        super().__init__(**kwargs)
+
+    @property
+    def current_electrodes(self):
+        """
+        The associated current electrode object (sources).
+        """
+        return self
+
+    @current_electrodes.setter
+    def current_electrodes(self, _):
+        """"""
+
+    @property
+    def potential_electrodes(self) -> PotentialElectrode | None:
+        """
+        The associated potential_electrodes (receivers)
+        """
+        if getattr(self, "_potential_electrodes", None) is None:
+            if self.metadata is not None and "Potential Electrodes" in self.metadata:
+                potential = self.metadata["Potential Electrodes"]
+                potential_entity = self.workspace.get_entity(potential)[0]
+
+                if isinstance(potential_entity, PotentialElectrode):
+                    self._potential_electrodes = potential_entity
+
+        return self._potential_electrodes
+
+    @potential_electrodes.setter
+    def potential_electrodes(self, potential_electrodes: PotentialElectrode):
+        if not isinstance(potential_electrodes, PotentialElectrode):
+            raise TypeError(
+                f"Provided potential_electrodes must be of type {PotentialElectrode}. "
+                f"{type(potential_electrodes)} provided."
+            )
+
+        metadata = {
+            "Current Electrodes": self.uid,
+            "Potential Electrodes": potential_electrodes.uid,
+        }
+
+        self.metadata = metadata
+        potential_electrodes.metadata = metadata
+
+        if isinstance(potential_electrodes.ab_cell_id, ReferencedData) and isinstance(
+            self.ab_cell_id, ReferencedData
+        ):
+            potential_electrodes.ab_cell_id.entity_type = self.ab_cell_id.entity_type
+
+    def add_default_ab_cell_id(self):
+        """
+        Utility function to set ab_cell_id's based on curve cells.
+        """
+        if getattr(self, "cells", None) is None or self.n_cells is None:
+            raise AttributeError(
+                "Cells must be set before assigning default ab_cell_id"
+            )
+
+        data = np.arange(self.n_cells) + 1
+        value_map = {ii: str(ii) for ii in range(self.n_cells + 1)}
+        value_map[0] = "Unknown"
+        ab_cell_id = self.add_data(
+            {
+                "A-B Cell ID": {
+                    "values": data,
+                    "association": "CELL",
+                    "primitive_type": "REFERENCED",
+                    "value_map": value_map,
+                }
+            }
+        )
+
+        if not isinstance(ab_cell_id, ReferencedData):
+            raise UserWarning("Could not create 'A-B Cell ID' data.")
+
+        ab_cell_id.entity_type.name = "A-B"
+        self._ab_cell_id = ab_cell_id
---
+++ packages/geoh5py/geoh5py-0.10.1.tar.gz/geoh5py-0.10.1/geoh5py/ui_json/forms.py
@@ -0,0 +1,351 @@
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+#  Copyright (c) 2025 Mira Geoscience Ltd.                                     '
+#                                                                              '
+#  This file is part of geoh5py.                                               '
+#                                                                              '
+#  geoh5py is free software: you can redistribute it and/or modify             '
+#  it under the terms of the GNU Lesser General Public License as published by '
+#  the Free Software Foundation, either version 3 of the License, or           '
+#  (at your option) any later version.                                         '
+#                                                                              '
+#  geoh5py is distributed in the hope that it will be useful,                  '
+#  but WITHOUT ANY WARRANTY; without even the implied warranty of              '
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the               '
+#  GNU Lesser General Public License for more details.                         '
+#                                                                              '
+#  You should have received a copy of the GNU Lesser General Public License    '
+#  along with geoh5py.  If not, see <https://www.gnu.org/licenses/>.           '
+# ''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''''
+
+
+from __future__ import annotations
+
+from enum import Enum
+from pathlib import Path
+from typing import Annotated, Any
+from uuid import UUID
+
+import numpy as np
+from pydantic import (
+    BaseModel,
+    ConfigDict,
+    field_serializer,
+    field_validator,
+    model_validator,
+)
+from pydantic.alias_generators import to_camel
+from pydantic.functional_validators import BeforeValidator
+
+from geoh5py.groups import Group
+from geoh5py.objects import ObjectBase
+from geoh5py.shared.validators import (
+    empty_string_to_uid,
+    to_class,
+    to_list,
+    to_path,
+    to_uuid,
+)
+from geoh5py.ui_json.validation import UIJsonError
+
+
+class DependencyType(str, Enum):
+    ENABLED = "enabled"
+    DISABLED = "disabled"
+
+
+class BaseForm(BaseModel):
+    """
+    Base class for uijson forms
+
+    :param label: Label for ui element.
+    :param value: The parameter's value.
+    :param optional: If True, ui element is rendered with a checkbox to
+        control the enabled state.
+    :param enabled: If False, ui element is rendered grey and value will
+        be written to file as None.
+    :param main: Controls whether ui element will render in the general
+        parameters tab (True) or optional parameters (False).
+    :param tooltip: String rendered on hover over ui element.
+    :param group: Grouped ui elements will be rendered within a box labelled
+        with the group name.
+    :param group_optional: If True, ui group is rendered with a checkbox that
+        controls the enabled state of all of the groups members
+    :param dependency: Name of parameter that controls the enabled or
+        visible state of the ui element.
+    :param dependency_type: Controls whether the ui element is enabled
+        or visible when the dependency is enabled if optional or True
+        if a bool type.
+    :param group_dependency: Name of parameter that controls the enabled
+        or visible state of the ui group.
+    :param group_dependency_type: Controls whether the ui group is
+        enabled or visible when the group dependency is enabled if
+        optional or True if a bool type.
+    """
+
+    model_config = ConfigDict(
+        extra="allow",
+        frozen=True,
+        populate_by_name=True,
+        loc_by_alias=True,
+        alias_generator=to_camel,
+    )
+
+    label: str
+    value: Any
+    optional: bool = False
+    enabled: bool = True
+    main: bool = True
+    tooltip: str = ""
+    group: str = ""
+    group_optional: bool = False
+    dependency: str = ""
+    dependency_type: DependencyType = DependencyType.ENABLED
+    group_dependency: str = ""
+    group_dependency_type: DependencyType = DependencyType.ENABLED
+
+    @property
+    def json_string(self):
+        return self.model_dump_json(exclude_unset=True, by_alias=True)
+
+    def flatten(self):
+        """Returns the data for the form."""
+        return self.value
+
+    def validate_data(self, params: dict[str, Any]):
+        """Validate the form data."""
+
+
+class StringForm(BaseForm):
+    """
+    String valued uijson form.
+    """
+
+    value: str = ""
+
+
+class BoolForm(BaseForm):
+    """
+    Boolean valued uijson form.
+    """
+
+    value: bool = True
+
+
+class IntegerForm(BaseForm):
+    """
+    Integer valued uijson form.
+    """
+
+    value: int = 1
+    min: float = -np.inf
+    max: float = np.inf
+
+
+class FloatForm(BaseForm):
+    """
+    Float valued uijson form.
+    """
+
+    value: float = 1.0
+    min: float = -np.inf
+    max: float = np.inf
+    precision: int = 2
+    line_edit: bool = True
+
+
+class ChoiceForm(BaseForm):
+    """
+    Choice list uijson form.
+    """
+
+    value: list[str]
+    choice_list: list[str]
+    multi_select: bool = False
+
+    @field_validator("value", mode="before")
+    @classmethod
+    def to_list(cls, value):
+        if not isinstance(value, list):
+            value = [value]
+        return value
+
+    @field_serializer("value", when_used="json")
+    def string_if_single(self, value):
+        if len(value) == 1:
+            value = value[0]
+        return value
+
+    @model_validator(mode="after")
+    def valid_choice(self):
+        bad_choices = []
+        for val in self.value:
+            if val not in self.choice_list:
+                bad_choices.append(val)
+        if bad_choices:
+            raise ValueError(f"Provided value(s): {bad_choices} are not valid choices.")
+
+        return self
+
+
+PathList = Annotated[
+    list[Path],
+    BeforeValidator(to_path),
+    BeforeValidator(to_list),
+]
+
+
+class FileForm(BaseForm):
+    """
+    File path uijson form
+    """
+
+    value: PathList
+    file_description: list[str]
+    file_type: list[str]
+    file_multi: bool = False
+
+    @field_serializer("value", when_used="json")
+    def to_string(self, value):
+        return ";".join([str(path) for path in value])
+
+    @field_validator("value")
+    @classmethod
+    def valid_file(cls, value):
+        bad_paths = []
+        for path in value:
+            if not path.exists():
+                bad_paths.append(path)
+        if any(bad_paths):
+            raise ValueError(f"Provided path(s) {bad_paths} does not exist.")
+        return value
+
+    @model_validator(mode="after")
+    def same_length(self):
+        if len(self.file_description) != len(self.file_type):
+            raise ValueError("File description and type lists must be the same length.")
+        return self
+
+    @model_validator(mode="before")
+    @classmethod
+    def value_file_type(cls, data):
+        bad_paths = []
+        for path in data["value"].split(";"):
+            if Path(path).suffix[1:] not in data["file_type"]:
+                bad_paths.append(path)
+        if any(bad_paths):
+            raise ValueError(f"Provided paths {bad_paths} have invalid extensions.")
+        return data
+
+
+MeshTypes = Annotated[
+    list[type[ObjectBase] | type[Group]],
+    BeforeValidator(to_class),
+    BeforeValidator(to_uuid),
+    BeforeValidator(to_list),
+]
+
+
+class ObjectForm(BaseForm):
+    """
+    Geoh5py object uijson form.
+    """
+
+    value: UUID = UUID("00000000-0000-0000-0000-000000000000")
+    mesh_type: MeshTypes
+
+    _empty_string_to_uid = field_validator("value", mode="before")(empty_string_to_uid)
+
+
+class Association(str, Enum):
+    """
+    Geoh5py object association types.
+    """
+
+    VERTEX = "Vertex"
+    CELL = "Cell"
+    FACE = "Face"
+
+
+class DataType(str, Enum):
+    """
+    Geoh5py data types.
+    """
+
+    INTEGER = "Integer"
+    FLOAT = "Float"
+    BOOLEAN = "Boolean"
+    REFERENCED = "Referenced"
+    VECTOR = "Vector"
+    DATETIME = "DateTime"
+    GEOMETRIC = "Geometric"
+    TEXT = "Text"
+
+
+class DataForm(BaseForm):
+    """
+    Geoh5py data uijson form.
+    """
+
+    value: UUID | float | int
+    parent: str
+    association: Association | list[Association]
+    data_type: DataType | list[DataType]
+    is_value: bool = False
+    property: UUID = UUID("00000000-0000-0000-0000-000000000000")
+    min: float = -np.inf
+    max: float = np.inf
+    precision: int = 2
+
+    @field_validator("property", mode="before")
+    @classmethod
+    def empty_string_to_uid(cls, val):
+        if val == "":
+            return UUID("00000000-0000-0000-0000-000000000000")
+        return val
+
+    @model_validator(mode="after")
+    def value_if_is_value(self):
+        if (
+            "is_value" in self.model_fields_set  # pylint: disable=unsupported-membership-test
+            and self.is_value
+        ):
+            if isinstance(self.value, UUID):
+                raise ValueError("Value must be numeric if is_value is True.")
+        return self
+
+    @model_validator(mode="after")
+    def property_if_not_is_value(self):
+        if (
+            "is_value" in self.model_fields_set  # pylint: disable=unsupported-membership-test
+            and "property" not in self.model_fields_set  # pylint: disable=unsupported-membership-test
+        ):
+            raise ValueError("A property must be provided if is_value is used.")
+        return self
+
+    def _validate_parent(self, params: dict[str, Any]):
+        """Validate form uid is a child of the parent object."""
+        child = None
+        if isinstance(self.value, UUID):
+            child = self.value
+        elif "property" in list(self.model_fields_set) and not self.is_value:
+            child = self.property
+
+        if child is not None:
+            if (
+                not isinstance(params[self.parent], ObjectBase)
+                or params[self.parent].get_entity(child)[0] is None
+            ):
+                raise UIJsonError(f"{child} data is not a child of {self.parent}.")
+
+    def validate_data(self, params: dict[str, Any]):
+        """Validate the form data."""
+        self._validate_parent(params)
+
+    def flatten(self):
+        """Returns the data for the form."""
+        if (
+            "is_value" in self.model_fields_set  # pylint: disable=unsupported-membership-test
+            and not self.is_value
+        ):
+            return self.property
+        return self.value
