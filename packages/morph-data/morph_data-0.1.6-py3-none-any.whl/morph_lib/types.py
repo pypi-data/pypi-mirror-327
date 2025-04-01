dc6eb86c6c3d22daafbf99f4021599734e3d648e
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 04:16:11 +0000

    Add package morph-data/0.1.6/morph_data-0.1.6-py3-none-any.whl

---
+++ packages/morph-data/morph_data-0.1.6-py3-none-any.whl/morph_lib/types.py
@@ -0,0 +1,46 @@
+from typing import Any, Dict, Optional
+
+from pydantic import BaseModel, Field
+
+
+class HtmlResponse(BaseModel):
+    value: str
+
+    def __init__(self, value: str):
+        super().__init__(value=value)
+
+
+class MarkdownResponse(BaseModel):
+    value: str
+
+    def __init__(self, value: str):
+        super().__init__(value=value)
+
+
+class ImageResponse(BaseModel):
+    value: str
+
+    def __init__(self, value: str):
+        super().__init__(value=value)
+
+
+class HtmlImageResponse(BaseModel):
+    html: Optional[str] = None
+    image: Optional[str] = None
+
+    def __init__(self, html: Optional[str] = None, image: Optional[str] = None):
+        super().__init__(html=html, image=image)
+
+
+class MorphChatStreamChunk(BaseModel):
+    text: Optional[str] = Field(default="")
+    content: Optional[str] = Field(default="")
+
+    @staticmethod
+    def is_chat_stream_chunk_json(data: Dict[str, Any]) -> bool:
+        return (
+            isinstance(data, dict)
+            and "data" in data
+            and isinstance(data["data"], list)
+            and all("text" in item and "content" in item for item in data["data"])
+        )
