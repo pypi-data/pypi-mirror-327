23f42dc8f1e1f6638cae4d6955e7f734a8681f60
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:31:23 +0000

    Add package AutoInspection/0.0.1/AutoInspection-0.0.1.tar.gz

---
+++ packages/AutoInspection/AutoInspection-0.0.1.tar.gz/AutoInspection-0.0.1/setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+from inspection import __version__ as version
+
+setup(
+    name='AutoInspection',
+    version=version,
+    packages=find_packages(),
+    install_requires=[],
+    url='https://github.com/hexs/Auto-Inspection-AI',
+)
