6ad8eb10d2e63a7cfc0bce7920918c0e11bf47fe
Bot <41898282+github-actions[bot]@users.noreply.github.com>
2025-04-01 03:27:34 +0000

    Add package y-cli/0.3.7/y_cli-0.3.7.tar.gz

---
+++ packages/y-cli/y_cli-0.3.7.tar.gz/y_cli-0.3.7/src/util.py
@@ -0,0 +1,12 @@
+import time
+
+def get_unix_timestamp() -> int:
+    """Get current time as 13-digit unix timestamp (milliseconds)"""
+    return int(time.time() * 1000)
+
+def get_iso8601_timestamp() -> str:
+    localtime = time.localtime()
+    offset = time.strftime("%z", localtime)
+    offset_with_colon = f"{offset[:3]}:{offset[3:]}"
+    formatted_time = time.strftime(f"%Y-%m-%dT%H:%M:%S{offset_with_colon}", localtime)
+    return formatted_time
